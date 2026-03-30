e8ae157a2a856a0ae98ca72e0386c64dd4280ba3
Asuka_zqs911 <3593503887@qq.com>
2026-03-30 17:42:13 +0800

    feat:添加cicd配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cicd_demo/',
+    base: process.env.NODE_ENV === 'production'
+    ? '/[testcicd]/'  // 替换为您的仓库名，如 '/my-vue-app/'
+    : '/',
 })
